659722f88e68eda2e181e52cea184d60795ca5d3
Riley Dalley <70665258+radalley@users.noreply.github.com>
2024-04-07 23:18:24 -0500

    Update dalapp.py

--- dalapp.py
+++ dalapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello world! Lets go Cardinals'
+    return 'Hello world! Let's go Cardinals'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
